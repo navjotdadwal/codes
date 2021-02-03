2f37e534b9b4d0df34291a14f17ff5e1845087eb
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-02-03 18:47:10 +0530

    Update A_Binary_Search.cpp

--- A_Binary_Search.cpp
+++ A_Binary_Search.cpp
@@ -1,20 +1,20 @@
-        int x;
-        cin>>x;
-        bool flag=false;
-        int l=0,r=n-1;
-        while(l<=r)
-        {
-          int mid=(l+r)/2;
-          if(a[mid]==x)
-          {
-            cout<<"YES\n";
-            flag=true;
-            break;
-          }
-          else if(a[mid]>x)
-            r=mid-1;
-          else 
-            l=mid+1;
-        }
-        if(!flag)
-          cout<<"NO\n";
+int x;
+cin>>x;
+bool flag=false;
+int l=0,r=n-1;
+while(l<=r)
+{
+  int mid=(l+r)/2;
+  if(a[mid]==x)
+  {
+    cout<<"YES\n";
+    flag=true;
+    break;
+  }
+  else if(a[mid]>x)
+    r=mid-1;
+  else 
+   l=mid+1;
+}
+if(!flag)
+  cout<<"NO\n";
