71ed3b3b3d0e8a16c2885daac435715136bfa22c
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-03-06 13:50:17 +0530

    Create MaxGreator to the left.cpp

---
+++ MaxGreator to the left.cpp
@@ -0,0 +1,18 @@
+vector<int> previousgreater(vector<int> h)
+{
+  vector<int> v(h.size()+1,-1);
+  stack<int> st;
+  for(int i=0;i<h.size();i++)
+  {
+    while(!st.empty() && h[st.top()]<=h[i])
+       st.pop();
+    if(!st.empty())
+    {
+      v[i]=st.top();
+      if(v[st.top()]!=-1)
+         v[i]=v[st.top()];
+    }
+    st.push(i);
+  }
+  return v;
+}
