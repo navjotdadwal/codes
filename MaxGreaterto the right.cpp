824d5ca0b583da5c554a6c1e80fceceb208d4efc
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-03-06 13:51:31 +0530

    Create MaxGreaterto the right.cpp

---
+++ MaxGreaterto the right.cpp
@@ -0,0 +1,18 @@
+vector<int> nextgreater(vector<int> h)
+{
+  vector<int> v(h.size()+1,-1);
+  stack<int> st;
+  for(int i=(int)h.size()-1;i>=0;i--)
+  {
+    while(!st.empty() && h[st.top()]<=h[i])
+       st.pop();
+    if(!st.empty())
+    {
+      v[i]=st.top();
+      if(v[st.top()]!=-1)
+         v[i]=v[st.top()];
+    }
+    st.push(i);
+ }
+ return v;
+}
