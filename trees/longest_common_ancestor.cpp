7ba7d856775a1c8b99efed1c8f6d98e6a6858313
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-03-14 16:49:27 +0530

    Update longest_common_ancestor.cpp

--- trees/longest_common_ancestor.cpp
+++ trees/longest_common_ancestor.cpp
@@ -1,12 +1,10 @@
 const int N = 3e5 + 5; 
 vector<int> adj[N];
-int p[N][35],dp[N][35],A[N][35],level[N];
+int p[N][35]level[N];
 void dfs(int v,int par)
 {
   p[v][0]=par;
   level[v]=level[par]+1;
-  for(int i=0;i<=30;i++)
-    dp[v][i]+=dp[par][i]+A[v][i];
   for(int to: adj[v])
   {
     if(to==par) continue;
