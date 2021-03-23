034392468d487bd44e1d790b32b1d1c87d3e0bd3
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-03-23 16:43:04 +0530

    Create Bridges.cpp

---
+++ Graph Theory/Bridges.cpp
@@ -0,0 +1,21 @@
+set<pair<int,int>> s;
+void dfs(int v,int p)
+{
+  vis[v]=1;
+  tin[v]=low[v]=timer++;
+  for(int to: adj[v])
+  {
+    if(to==p) continue;
+    if(vis[to])
+    {
+      low[v]=min(low[v],tin[to]);
+    } 
+    else 
+    {
+      dfs(to);  
+      low[v]=min(low[v],low[to]);
+      if(low[v]<tin[to])
+        s.insert({to,v});
+    } 
+  }
+}
