351f75f8f1aad544812690b838ad4a2abd08254a
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-03-23 16:46:20 +0530

    Create BridgesInO(n+m).cpp

---
+++ Graph Theory/BridgesInO(n+m).cpp
@@ -0,0 +1,29 @@
+
+set<pair<int,int>> s;
+void dfs(int v,int p)
+{
+  vis[v]=1;
+  tin[v]=low[v]=timer++;
+  for(int to: adj[v])
+  {
+    if(to==p) continue;
+    if(vis[to])
+    {
+      low[v]=min(low[v],tin[to]);
+    } 
+    else 
+    {
+      dfs(to);  
+      low[v]=min(low[v],low[to]);
+      if(low[v]<tin[to])
+        s.insert({to,v});
+    } 
+  }
+}
+for(int i=1;i<=n;i++)
+{
+  if(!vis[i])
+  {
+    dfs(i);
+  }
+}
