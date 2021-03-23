71c5267f6ad44532a7245cb19d4cec9dc33aa50e
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-03-23 19:50:46 +0530

    Update BellmonFord.cpp

--- Graph Theory/BellmonFord.cpp
+++ Graph Theory/BellmonFord.cpp
@@ -1,11 +1,12 @@
-for (int i = 1; i <= n; i++) {
-dist[i] = INF;
-}
+for (int i = 1; i <= n; i++) 
+  dist[i] = INF;
 distance[x] = 0;
-for (int i = 1; i <= n-1; i++) {
-for (auto e : edges) {
-int a, b, w;
-tie(a, b, w) = e;
-dist[b] = min(dist[b], dist[a]+w);
-}
+for (int i = 1; i <= n-1; i++) 
+{
+  for (auto e : edges) 
+  {
+    int a, b, w;
+    tie(a, b, w) = e;
+    dist[b] = min(dist[b], dist[a]+w);
+  }
 }
