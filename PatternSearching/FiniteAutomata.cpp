38d2270a5d2eb8a82fb13c5e7f6de13cb8bb1022
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-05-07 12:43:59 +0530

    Create FiniteAutomata.cpp

---
+++ PatternSearching/FiniteAutomata.cpp
@@ -0,0 +1,50 @@
+Reference: https://www.geeksforgeeks.org/pattern-searching-set-5-efficient-constructtion-of-finite-automata/
+
+#define NO_OF_CHARS 256
+ 
+/* This function builds the TF table
+which represents Finite Automata for a
+given pattern */
+void computeTransFun(string pat, int M, int TF[][NO_OF_CHARS])
+{
+    int i, lps = 0, x;
+ 
+    // Fill entries in first row
+    for (x = 0; x < NO_OF_CHARS; x++)
+        TF[0][x] = 0;
+    TF[0][pat[0]] = 1;
+ 
+    // Fill entries in other rows
+    for (i = 1; i <= M; i++) {
+        // Copy values from row at index lps
+        for (x = 0; x < NO_OF_CHARS; x++)
+            TF[i][x] = TF[lps][x];
+ 
+        // Update the entry corresponding to this character
+        TF[i][pat[i]] = i + 1;
+ 
+        // Update lps for next row to be filled
+        if (i < M)
+            lps = TF[lps][pat[i]];
+    }
+}
+ 
+/* Prints all occurrences of pat in txt */
+void search(string pat,string txt)
+{
+    int M = pat.length();
+    int N = txt.length();
+ 
+    int TF[M + 1][NO_OF_CHARS];
+ 
+    computeTransFun(pat, M, TF);
+ 
+    // process text over FA.
+    int i, j = 0;
+    for (i = 0; i < N; i++) {
+        j = TF[j][txt[i]];
+        if (j == M) {
+            cout << "pattern found at index " << i - M + 1 << endl;
+        }
+    }
+}
